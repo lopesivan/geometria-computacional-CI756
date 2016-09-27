47018910d96639d50a109e21ebe034b242944b96
Gustavo Higuchi <gghiguchi@gmail.com>
2016-09-27 11:08:20 -0300

    trabalho 2 - concluido

--- trabalhos/dcel3D/Dcel.py
+++ trabalhos/dcel3D/Dcel.py
@@ -9,19 +9,14 @@ class Ponto(object):
         self.x = x
         self.y = y
         self.z = z
+        self.edge = None
     def __repr__(self):
         return "%s (%s,%s,%s)" % (self.id, self.x, self.y, self.z)
-    #def __eq__(self, other):
-    #    if self.x == other.x and self.y == other.y and self.z == other.z:
-    #        return True
-    #    else:
-    #        return False
 
 class Face(object):
-    def __init__(self, id, inner, vertices):
+    def __init__(self, id, inner):
         self.id = id
         self.inner = inner
-        self.vertices = vertices
     def __repr__(self):
         return "%s - edge: %s" % (self.id, self.inner)
 
@@ -33,15 +28,12 @@ class Edge(object):
         self.twin = None
         self.next = None
         self.prev = None
-        self.v1 = v1
-        self.v2 = v2
-        self.helper = None
 
     def __repr__(self):
-        return "%s %s %s" % (self.id, self.v1.id, self.v2.id)
+        return "%s origem: %s" % (self.id, self.orig.id)
 
 class Poliedro(object):
-    def __init__(self, id, vertices, faces):
+    def __init__(self, id, vertices, faces, vertices_faces):
         self.id = id
         self.vertices = vertices
         self.faces = faces
@@ -49,22 +41,25 @@ class Poliedro(object):
 
         for i in xrange(len(faces)):
             f = faces[i]
+            v = vertices_faces.pop(0)
             edges = []
 
-            for j in xrange(0, len(f.vertices)):
+            for j in xrange(0, len(v)):
                 # busco na lista de arestas que já estão no
                 # no polígono, para saber qual é uma aresta compatilhada
                 # pelo vértice de origem e destino
-                e_shared = shared_edge(self.edges, vertices[f.vertices[j-1]-1], vertices[f.vertices[j]-1])
+                e_shared = shared_edge(self.edges, vertices[v[j-1]-1], vertices[v[j]-1])
                 if e_shared:
-                    # caso possua, a aresta simetrica recebe a face 'f'
-                    # então checa se a twin possue uma face, se sim,
+                    # caso possua, a semi-aresta recebe a face 'f'
+                    # então checa se possue uma face, se sim,
                     # erro, pois existem mais de uma face compartilhando
-                    # a mesma aresta
+                    # a mesma semi-aresta, portanto não é orientável
                     if e_shared.face != None:
-                        print "Erro! Aresta com mais de uma face!"
-                        return False
+                        print 'não orientável'
+                        exit(1)
 
+                    # guardo o indice da aresta para posteriormente
+                    # definir quem é o próximo e o anterior da aresta
                     edges.append(e_shared.id)
                     e_shared.face = f
                     # e a primeira aresta da face é a aresta compartilhada
@@ -72,13 +67,20 @@ class Poliedro(object):
                     if f.inner == None:
                         f.inner = e_shared
                 else:
-                    e = Edge(len(self.edges)+1, vertices[f.vertices[j-1]-1], vertices[f.vertices[j]-1], f)
-                    e_twin = Edge(len(self.edges)+2, vertices[f.vertices[j]-1], vertices[f.vertices[j-1]-1], None)
+                    # crio a aresta e sua simétrica
+                    e = Edge(len(self.edges)+1, vertices[v[j-1]-1], vertices[v[j]-1], f)
+                    e_twin = Edge(len(self.edges)+2, vertices[v[j]-1], vertices[v[j-1]-1], None)
                     e.twin = e_twin
                     e_twin.twin = e
+
+                    # define a aresta do vertice caso não tenha ainda
+                    if vertices[v[j-1]-1].edge == None:
+                        self.vertices[v[j-1]-1].edge = e
+
+                    # insiro na lista de aresta do poliedro
                     self.edges.append(e)
-                    edges.append(e.id)
                     self.edges.append(e_twin)
+                    edges.append(e.id)
 
             for j in xrange(0, len(edges)):
                 self.edges[edges[j-1]-1].next = self.edges[edges[j]-1]
@@ -90,8 +92,17 @@ class Poliedro(object):
             if f.inner == None:
                 f.inner = self.edges[-2]
 
-
-
+#-----------------------------------------------#
+# Verifica se o poliedro está aberto
+# Entrada: um poliedro
+# Saida: True caso esteja aberto,
+#        false caso contrário
+#-----------------------------------------------#
+def aberto(p):
+    for e in p.edges:
+        if e.face == None:
+            return True
+    return False
 
 #-----------------------------------------------#
 # Verifica se a aresta v1->v2 existe
@@ -99,16 +110,21 @@ class Poliedro(object):
 #          e o vértice de destino
 # Saida: a aresta v1->v2,
 #        false caso nao exista
-#-----------------------------------------------#
+#-----------------------------------------------#2
 def shared_edge(edges, v1, v2):
     for e in edges:
-        if e.v1.id == v1.id and e.v2.id == v2.id:
+        if e.orig.id == v1.id and e.twin.orig.id == v2.id:
             return e
     return False
 
-def show_data(p):
+def show_data(p, vertices):
+    print len(p.vertices), len(p.edges)/2, len(p.faces)
+    for v in p.vertices:
+        print v.x, v.y, v.z, v.edge.id
+    for vertex in vertices:
+        print ' '.join(str(v) for v in vertex   )
     for f in p.faces:
         print f.inner.id
     for e in p.edges:
-        print e.orig, e.twin.id, e.face.id, e.next.id, e.prev.id
+        print e.orig.id, e.twin.id, e.face.id, e.next.id, e.prev.id
     return True
