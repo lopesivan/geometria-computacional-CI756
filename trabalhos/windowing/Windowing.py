16e4edf102daf756714f0a2edb7dd99fcf27f96a
Gustavo Higuchi <gghiguchi@gmail.com>
2016-11-09 20:42:13 -0200

    query in a 2D Range Tree - got bugs

--- trabalhos/windowing/Windowing.py
+++ trabalhos/windowing/Windowing.py
@@ -23,7 +23,6 @@ class Window(object):
         self.y2 = y2
 
 def RangeTree(P):
-    #T = ArvBinBusca( coordenada_y(P) )
     T = ArvBinBusca( P, 0 )
     if len(P) == 0:
         return 
@@ -31,22 +30,137 @@ def RangeTree(P):
         v = Node( P[0].x, P[0] )
         v.tree_assoc = T.root 
     else:
+        #--------------------------------------------#
+        # Divide o conjunto de pontos com base na 
+        # mediana da coordenada x em duas listas, esquerda
+        # e direita, onde pontos com coordenada menor
+        # que x_mid na esquerda, e maiores à direita
         left, right = split(P)
-        x_mid = left.pop()
+        # guarda o nó do meio, no caso, o maior da lista
+        # da esquerda
+        x_mid = left.pop(getRootIndex(left))
+        
         v_left = RangeTree(left)
         v_right = RangeTree(right)
         v = Node( x_mid.x, x_mid )
         v.left = v_left
         v.right = v_right
+
+        # guarda a raiz da árvore balanceada associada
+        # no nó do meio  
+        v.tree_assoc = T.root
+        #--------------------------------------------#
+
+        v.height = max(height(v.left), height(v.right)) + 1
+
     return v
 
-#-----------------------------------------------#
+def query2DRangeTree( node, x, x1, y, y1 ):
+    response = []
+    v_split = findSplitNode( node, x, x1 )
+    if leaf( v_split ):
+        if (x < v_split.point.x and v_split.point.x < x1 and 
+            y < v_split.point.y and v_split.point.y < y1):
+                response.append( v_split )
+    else:
+        v = v_split.left
+        while not leaf( v ):
+            if x <= v.point.x:
+                response.extend(query1DRangeTree( v.tree_assoc, y, y1 ))
+                v = v.left
+            else:
+                v = v.right
+        if v:
+            if (x < v_split.point.x and v_split.point.x < x1 and
+                y < v_split.point.y and v_split.point.y < y1):
+                response.append( v_split )
+
+        v = v_split.right
+        while not leaf( v ):
+            if v.point.x <= x1:
+                response.extend(query1DRangeTree( v.tree_assoc, y, y1 ))
+                v = v.right
+            else:
+                v = v.left
+        if v:
+            if (x < v.point.x and v.point.x < x1 and
+                y < v.point.y and v.point.y < y1):
+                response.append( v_split )
+
+    return response
+
+
+def query1DRangeTree( node, x, x1 ):
+    response = []
+    v_split =  findSplitNode(node, x, x1)
+    if leaf( v_split ):
+        if x <= v_split.key and v_split.key <= x1: 
+            response.append( v_split )
+    else:
+        #--------------------------------------------#
+        # percorre a subárvore à esquerda do nó split
+        # e reporta todos os pontos à direita.
+        v = v_split.left
+        while not leaf(v):
+            if x < v.key:
+                reportSubtree( v.right, response )
+                v = v.left
+            else:
+                v = v.right
+        if x <= v.key: 
+            response.append( v )
+        
+        # semelhante para o limite de x' 
+        v = v_split.right
+        while not leaf(v):
+            if v.key <= x1:
+                reportSubtree( v.left, response )
+                v = v.right
+            else:
+                v = v.left
+        if v and v.key <= x1:
+            response.append( v )
+        #--------------------------------------------#
+
+    return response
+
+#---------------------------------------------------#
+# Função que concatena recursivamente os nós na 
+# resposta que será reportada
+#
+# Entrada: um nó e uma lista de resposta
+# Saída: NA
+#---------------------------------------------------#
+def reportSubtree( node, response ):
+    if not node:
+        return True
+    reportSubtree(node.left, response)
+    reportSubtree(node.right, response)
+    response.append(node)
+    return True
+
+#---------------------------------------------------#
+# Função que encontra e retorna o nó split
+#
+# Entrada: nó raiz e um intervalo [x:x']
+# Saída: nó split
+#---------------------------------------------------#
+def findSplitNode( node, x, x1 ):
+    v = node
+    while not leaf(v) and (x1 <= v.key or x > v.key):
+        if x1 <= v.key:
+            v = v.left
+        else:
+            v = v.right
+    return v
+
+#---------------------------------------------------#
 # Função que divide uma coleção de pontos em
 # menores que a mediana das coordenas-x
 #
 # Entrada: uma coleção de pontos
 # Saída: duas listas de pontos, P_left e P_right
-#-----------------------------------------------#
+#---------------------------------------------------#
 def split(points):
     left = []
     right = []
@@ -61,6 +175,34 @@ def split(points):
 
     return left, right
 
+#---------------------------------------------------#
+# Função que devolve o maior elemento de uma lista
+# de pontos com base na coordenada-x
+#
+# Entrada: uma coleção de pontos
+# Saída: índice do maior elemento
+#---------------------------------------------------#
+def getRootIndex(t):
+    maior = 0
+    i = 0
+    for e in t:
+        if t[maior].x < e.x:
+            maior = i
+        i += 1
+    return maior
+
+#---------------------------------------------------#
+# Função identifica se o nó é folha
+#
+# Entrada: um nó 
+# Saída: True se for um nó folha,
+#        False caso contrário
+#---------------------------------------------------#
+def leaf( node ):
+    if not node or (not node.left and not node.right):
+        return True
+    return False
+
 def median(points):
     total_x = 0
     for p in points:
@@ -73,9 +215,9 @@ def coordenada_y(P):
         P_y.append( point.y )
     return P_y
 
-
-#--------- Funções para árvore AVL -------------#
-
+#---------------------------------------------------#
+#------------- Funções para árvore AVL -------------#
+#---------------------------------------------------#
 class Node(object):
     def __init__(self, key, point):
         self.key = key
@@ -87,12 +229,13 @@ class Node(object):
 
     def __repr__(self):
         return "%s (%s,%s) %s" % (self.key, self.point.x, self.point.y, self.height)
-#-----------------------------------------------#
+
+#---------------------------------------------------#
 # Entrada: uma coleção de pontos, e um booleano
 #          indicando por qual coordenada será 
 #          usada como chave (true para eixo x)
 # Saída: uma arvore binária balanceada
-#-----------------------------------------------#
+#---------------------------------------------------#
 class ArvBinBusca(object):
     def __init__(self, P, x_coord):
         self.root = None
@@ -102,13 +245,16 @@ class ArvBinBusca(object):
         else:
             for point in P:
                 self.root = insert_y( self.root, point )
+    def __repr__(self):
+        return "%s (%s,%s) %s" % (self.root.key, self.root.point.x, self.root.point.y, self.root.height)
 
-#-----------------------------------------------#
+#---------------------------------------------------#
 # Função recursiva para inserir um ponto na árvore
 # com base na coordenada y
+#
 # Entrada: o nó raiz e o ponto a ser inserido
 # Saída: uma arvore binária balanceada
-#-----------------------------------------------#
+#---------------------------------------------------#
 def insert_y(node, point):
     if not node:
         n = Node( point.y, point )
@@ -135,12 +281,13 @@ def insert_y(node, point):
 
     return node
 
-#-----------------------------------------------#
+#---------------------------------------------------#
 # Função recursiva para inserir um ponto na árvore
 # com base na coordenada x
+#
 # Entrada: o nó raiz e o ponto a ser inserido
 # Saída: uma arvore binária balanceada
-#-----------------------------------------------#
+#---------------------------------------------------#
 def insert_x(node, point):
     if not node:
         n = Node( point.x, point )
@@ -167,9 +314,9 @@ def insert_x(node, point):
 
     return node
 
-#-----------------------------------------------#
+#---------------------------------------------------#
 # Funções helpers para manter balanceamento da AVL
-#-----------------------------------------------#
+#---------------------------------------------------#
 def rot_right(y):
     x = y.left
     y.left = x.right
@@ -202,8 +349,6 @@ def imprime_arv(node):
     if not node:
         return
     imprime_arv(node.left)
-    if node.tree_assoc:
-        imprime_arv(node.tree_assoc)
     print node
     imprime_arv(node.right)
 
--- trabalhos/windowing/test.py
+++ trabalhos/windowing/test.py
@@ -4,13 +4,15 @@ from Windowing import *
 
 x = []
 
-x.append(Point(10,10))
-x.append(Point(20,20))
-x.append(Point(30,4))
+x.append(Point(6,6))
+x.append(Point(8,7))
+x.append(Point(10,4))
 x.append(Point(5,2))
-x.append(Point(3,15))
+x.append(Point(3,5))
+
 
 arv = RangeTree(x)
 
-
-#imprime_arv(arv.root)
+r = query2DRangeTree(arv, 5, 9, 5, 9)
+print r
+#imprime_arv(arv)
